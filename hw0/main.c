833a3909515b604e6f581135d3faef777e75e735
Donny Reynolds <donnywith@gmail.com>
2015-01-27 22:03:17 -0800

    exits with 0 code

--- hw0/main.c
+++ hw0/main.c
@@ -10,4 +10,5 @@ int main() {
     printf("process limit: %d\n", (int)lim.rlim_max);
     getrlimit(RLIMIT_NOFILE, &lim);
     printf("max file descriptors: %d\n", (int)lim.rlim_max);
+    return 0;
 }
