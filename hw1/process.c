0461d36d24d185ecb49b64954229c0afaf9f7002
Donny Reynolds <donnywith@gmail.com>
2015-02-08 21:21:24 -0800

    migrates structure to conform process interface

--- hw1/process.c
+++ hw1/process.c
@@ -11,7 +11,7 @@
  * If the shell is in interactive mode and the process is a foreground process,
  * then p should take control of the terminal.
  */
-void launch_process(process *p) {
+ void launch_process(process *p) {
   if (shell_is_interactive) {
     // put this process into its own process group
     pid_t pid = p->pid;
@@ -26,7 +26,11 @@ void launch_process(process *p) {
     }
     // restore signal handling for this process
     signal(SIGINT, SIG_DFL);
+    signal(SIGQUIT, SIG_DFL);
     signal(SIGTSTP, SIG_DFL);
+    signal(SIGTTIN, SIG_DFL);
+    signal(SIGTTOU, SIG_DFL);
+    signal(SIGCHLD, SIG_DFL);
   }
   // redirect in >
   if (p->stdin != STDIN_FILENO) {
@@ -51,12 +55,12 @@ void launch_process(process *p) {
  * is in interactive mode. If the cont argument is true, send the process
  * group a SIGCONT signal to wake it up.
  */
-void put_process_in_foreground (process *p, int cont) {
+ void put_process_in_foreground (process *p, int cont) {
   /** YOUR CODE HERE */
-}
+ }
 
 /* Put a process in the background. If the cont argument is true, send
  * the process group a SIGCONT signal to wake it up. */
-void put_process_in_background (process *p, int cont) {
+ void put_process_in_background (process *p, int cont) {
   /** YOUR CODE HERE */
-}
+ }
--- hw1/shell.c
+++ hw1/shell.c
@@ -9,6 +9,7 @@
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <stdbool.h>
 
 #define FALSE 0
 #define TRUE 1
@@ -18,9 +19,6 @@
 #include "parse.h"
 #include "process.h"
 #include "shell.h"
-
-static struct sigaction SIGINT_backup;
-static struct sigaction SIGTSTP_backup;
 
 int asprintf(char **strp, const char *fmt, ...);
 
@@ -96,35 +94,28 @@ void init_shell()
     tcgetattr(shell_terminal, &shell_tmodes);
   }
   /** YOUR CODE HERE */
-  struct sigaction ignore;
-  ignore.sa_handler = SIG_IGN;
-  sigaction(SIGTSTP, &ignore, &SIGTSTP_backup);
-  sigaction(SIGINT, &ignore, &SIGINT_backup);
+  signal(SIGINT, SIG_IGN);
+  signal(SIGQUIT, SIG_IGN);
+  signal(SIGTSTP, SIG_IGN);
+  signal(SIGTTIN, SIG_IGN);
+  signal(SIGTTOU, SIG_IGN);
+  signal(SIGCHLD, SIG_IGN);
 }
 
 /**
  * Add a process to our process list
  */
-void add_process(process* p)
-{
+ void add_process(process* p)
+ {
   /** YOUR CODE HERE */
-}
+ }
 
-/**
- * Creates a process given the inputString from stdin
- */
-process* create_process(char* inputString) {
-  process *proc = malloc(sizeof(process));
-  tok_t *t = getToks(inputString); /* break the line into tokens */
-  int i;
-  for (i=0; t[i]; ++i){};
-  proc->argc = i;
-  proc->argv = t;
-  return proc;
-}
 
-char* resolve_path(char *fname) {
-  char *PATH = getenv("PATH");
+
+ char* resolve_path(char *fname) {
+  char *ORIGINAL_PATH = getenv("PATH");
+  char *PATH;
+  asprintf(&PATH, "%s", ORIGINAL_PATH);
   tok_t *t = getToks(PATH);
   int i = 0;
   for (i=0; t[i]; i++) {
@@ -138,87 +129,121 @@ char* resolve_path(char *fname) {
   return NULL;
 }
 
-void setup_io(tok_t *t, tok_t* *cmds, process *proc) {
-  int i;
-  for (i=0; t[i]; ++i) {
-    if (strcmp(t[i], ">") == 0) {
-      tok_t *commands = malloc(sizeof(tok_t)*i);
-      memcpy(commands, t, sizeof(tok_t)*i);
-      *cmds = commands;
-      tok_t outfile = t[i+1];
-      fflush(stdout);
-      FILE *out_desc = fopen(outfile, "w");
-      dup2(fileno(out_desc), STDOUT_FILENO);
-      fclose(out_desc);
-      proc->stdout = fileno(stdout);
-      tcgetattr(fileno(stdout), &(proc->tmodes));
-      return;
-    }
-    if (strcmp(t[i], "<") == 0) {
-      tok_t *commands = malloc(sizeof(tok_t)*i);
-      memcpy(commands, t, sizeof(tok_t)*i);
-      *cmds = commands;
-      tok_t infile = t[i+1];
-      fflush(stdin);
-      int in_desc = open(infile, O_RDONLY);
-      dup2(in_desc, STDIN_FILENO);
-      close(in_desc);
-      proc->stdin = fileno(stdin);
-      tcgetattr(fileno(stdin), &(proc->tmodes));
-      return;
+// void setup_io(tok_t *t, tok_t* *cmds, process *proc) {
+//   int i;
+//   for (i=0; t[i]; ++i) {
+//     if (strcmp(t[i], ">") == 0) {
+//       tok_t *commands = malloc(sizeof(tok_t)*i);
+//       memcpy(commands, t, sizeof(tok_t)*i);
+//       *cmds = commands;
+//       tok_t outfile = t[i+1];
+//       fflush(stdout);
+//       FILE *out_desc = fopen(outfile, "w");
+//       dup2(fileno(out_desc), STDOUT_FILENO);
+//       fclose(out_desc);
+//       proc->stdout = fileno(stdout);
+//       tcgetattr(fileno(stdout), &(proc->tmodes));
+//       return;
+//     }
+//     if (strcmp(t[i], "<") == 0) {
+//       tok_t *commands = malloc(sizeof(tok_t)*i);
+//       memcpy(commands, t, sizeof(tok_t)*i);
+//       *cmds = commands;
+//       tok_t infile = t[i+1];
+//       fflush(stdin);
+//       int in_desc = open(infile, O_RDONLY);
+//       dup2(in_desc, STDIN_FILENO);
+//       close(in_desc);
+//       proc->stdin = fileno(stdin);
+//       tcgetattr(fileno(stdin), &(proc->tmodes));
+//       return;
+//     }
+//   }
+//   *cmds = t;
+// }
+/**
+ * Creates a process given the inputString from stdin
+ */
+ process* create_process(char* inputString) {
+  
+  // create process on heap
+  process *p = malloc(sizeof(process));
+  // tokenize command line input
+  tok_t *t = getToks(inputString); /* break the line into tokens */
+  // figure out argv, argc and io
+  int argc;
+  bool io_found = false;
+  p->argv = t;
+  p->stdin = STDIN_FILENO;
+  p->stdout = STDOUT_FILENO;
+  p->stderr = STDERR_FILENO;
+  for (argc=0; t[argc]; ++argc) {
+    if (!io_found) {
+      // figure out argv
+      if (strcmp(t[argc], ">")==0 || strcmp(t[argc], "<")==0) {
+        // copy every token before the pipe sign
+        p->argv = malloc(sizeof(tok_t)*argc);
+        memcpy(p->argv, t, sizeof(tok_t)*argc);
+        io_found = true;
+      }
+      // figure out io
+      if (strcmp(t[argc], ">")==0) {
+        tok_t outfile = t[argc+1];
+        p->stdout = fileno(fopen(outfile, "w"));
+      } else if (strcmp(t[argc], "<")==0) {
+        tok_t infile = t[argc+1];
+        p->stdin = fileno(fopen(infile, "r"));
+      }
     }
   }
-  *cmds = t;
+  // set argument count
+  p->argc = argc;
+  // resolve file path
+  bool access_ok = (access(p->argv[0], F_OK) != -1);
+  if (!access_ok) {
+    p->argv[0] = resolve_path(p->argv[0]);
+    if (!p->argv[0]) {
+      return NULL;
+    }
+  }
+  return p;
 }
 
 void run_program(tok_t *t, char *s) {
   // execute another program specified in command
   int exit_status;
-  process* proc = create_process(s);
-  pid_t pid = fork();
+  process* proc;
+  if ((proc = create_process(s)) != NULL) {
+    pid_t pid = fork();
   // both parent and child should set put the child into its own process group
-  
-
-  if (pid == 0) {
-    // put into its own process group
-    pid_t child_pid = getpid();
-    if (setpgid(child_pid, child_pid) < 0) {
-      perror("Couldn't put the program in its own process group");
-      exit(1);
-    }
-    // restore signal behavior
-    // sigaction(SIGTSTP, &SIGTSTP_backup, NULL);
-    // sigaction(SIGINT, &SIGINT_backup, NULL);
-    // Take control of the terminal
-    // tcsetpgrp(proc->stdin, getppid());
-    // tcgetattr(proc->stdin, &(proc->tmodes));
-
-    tok_t *commands;
-    setup_io(t, &commands, proc);
-
-    char *file_path = commands[0];
-    // try to resolve the file
-    if (access(file_path, F_OK) == -1 && !(file_path = resolve_path(file_path))) {
-      // cannot find file anywhere
-      proc->stopped = 1;
-      exit(0);
-    }
-    execv(file_path, commands);
-  } else {
-    proc->pid = pid;
-    // waits for child to finish
-    waitpid(pid, &exit_status, 0);
-    if (WIFEXITED(exit_status)) {
-      proc->status = WEXITSTATUS(exit_status);
-      if (WIFSTOPPED(exit_status)) {
-        proc->stopped = 1;
-      } else {
-        proc->completed = 1;
+    if (pid == 0) {
+      proc->pid = getpid();
+      launch_process(proc);
+    } else {
+      proc->pid = pid;
+      if (shell_is_interactive) {
+        setpgid(proc->pid, proc->pid);
       }
+      waitpid(proc->pid, &exit_status, 0);
+      tcsetpgrp(shell_terminal, shell_pgid);
+      tcgetattr(shell_terminal, &shell_tmodes);
+      tcsetattr(shell_terminal, TCSADRAIN, &shell_tmodes);
+    // init_shell();
+    // if (WIFEXITED(exit_status)) {
+    //   proc->status = WEXITSTATUS(exit_status);
+    //   if (WIFSTOPPED(exit_status)) {
+    //     proc->stopped = 1;
+    //   } else {
+    //     proc->completed = 1;
+    //   }
+    // }
     }
-    // tcsetpgrp(shell_terminal, shell_pgid);
-    // tcgetattr(shell_terminal, &shell_tmodes);
+    /* Clean up after pipes.  */
+    // if (proc->stdin != STDIN_FILENO) close(proc->stdin);
+    // if (proc->stdout != STDOUT_FILENO) close(proc->stdout);
+    // if (proc->stderr != STDERR_FILENO) close(proc->stderr);
   }
+  
 }
 
 int shell (int argc, char *argv[]) {
